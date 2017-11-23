d80affb34a7b19420beff9538ce71a7be2a61c67
Candice Johnson <candicelj1708@gmail.com>
2017-11-22 23:47:08 -0600

    Php test seven.

--- populator.php
+++ populator.php
@@ -31,18 +31,14 @@ function heroDropDown(){
 }
 
 function teamDropDown(){
-	try {
 	$team_one = "team1";
 	$team_two = "team2";
 	$team_three = "team3";
 	$team_four = "team4";
 	echo "<option value='" . $team_one . "'>" . $team_one . "</option>";
-	echo "<option value='" . $team_two + "'>" . $team_two . "</option>";
-	echo "<option value='" . $team_three + "'>" . $team_three . "</option>";
-	echo "<option value='" . $team_four + "'>" . $team_four . "</option>";
-	} catch (Exception $e){
-		echo 'Caught exception: ', $e->getMessage(), "\n";
-	}
+	echo "<option value='" . $team_two . "'>" . $team_two . "</option>";
+	echo "<option value='" . $team_three . "'>" . $team_three . "</option>";
+	echo "<option value='" . $team_four . "'>" . $team_four . "</option>";
 }
 
 function gearDropDown(){
