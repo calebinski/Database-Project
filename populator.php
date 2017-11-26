6eb542d23463603f2c65d9875c40af311cc58d18
Candice Johnson <candicelj1708@gmail.com>
2017-11-25 18:43:33 -0600

    Php pulling from SWL.

--- populator.php
+++ populator.php
@@ -27,26 +27,31 @@ function editTeamModal(){
 }
 
 function heroDropDown(){
-	
+	$heroes = queryMysql("SELECT Character_Name FROM player_character WHERE Owner_ID=3");
+	while ($hero = $heroes->fetch_assoc()){
+		echo "<option>" . $hero['Character_Name'] . "</option>";	
+	}
 }
 
 function teamDropDown(){
-	$team_one = "team1";
-	$team_two = "team2";
-	$team_three = "team3";
-	$team_four = "team4";
-	echo "<option value='" . $team_one . "'>" . $team_one . "</option>";
-	echo "<option value='" . $team_two . "'>" . $team_two . "</option>";
-	echo "<option value='" . $team_three . "'>" . $team_three . "</option>";
-	echo "<option value='" . $team_four . "'>" . $team_four . "</option>";
+	$teams = queryMysql("SELECT Team_Name FROM teams");
+	while ($team = $teams->fetch_assoc()){
+		echo "<option>" . $team['Team_Name'] . "</option>";	
+	}
 }
 
 function gearDropDown(){
-	
+	$gear = queryMysql("SELECT Gear_Name FROM gear");
+	while ($item = $gear->fetch_assoc()){
+		echo "<option>" . $item['Gear_Name'] . "</option>";	
+	}
 }
 
 function enemyDropDown(){
-	
+	$enemies = queryMysql("SELECT Enemy_Name FROM enemy");
+	while ($enemy = $enemies->fetch_assoc()){
+		echo "<option>" . $enemy['Enemy_Name'] . "</option>";	
+	}
 }
 
 ?>
--- userQueries.php
+++ userQueries.php
@@ -2,6 +2,36 @@
 
 include 'sqlConnect.php';
 
+function addHero(){
+	
+}
 
+function editHero(){
+	
+}
+
+function deleteHero(){
+	
+}
+
+function createTeam(){
+	
+}
+
+function joinTeam(){
+	
+}
+
+function editTeam(){
+	
+}
+
+function deleteTeam(){
+	
+}
+
+function calculateCombat(){
+	
+}
 
 ?>
